9492f522784886b0b49fe0dc0296d7b984f79b45
unknown <bramsosifo@gmail.com>
2017-02-10 14:03:54 -0500

    Home Page completed

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,43 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// Global Route Variables
+let currentDate = new Date();
+currentDate = currentDate.toLocaleTimeString();
+
+/* GET home page. wildcard */
+router.get('/', (req, res, next) => {
+    res.render('content/index', {
+        title: 'Home'
+    });
+});
+
+/* GET about page. */
+router.get('/about', (req, res, next) => {
+    res.render('content/about', {
+        title: 'About'
+    });
+});
+
+/* GET products page. */
+router.get('/projects', (req, res, next) => {
+    res.render('content/projects', {
+        title: 'Projects'
+    });
+});
+
+/* GET services page. */
+router.get('/services', (req, res, next) => {
+    res.render('content/services', {
+        title: 'Services'
+    });
+});
+
+/* GET contact page. */
+router.get('/contact', (req, res, next) => {
+    res.render('content/contact', {
+        title: 'Contact'
+    });
 });
 
 module.exports = router;
